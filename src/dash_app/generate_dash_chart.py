e987f4a6fcf8e12e309221b0285df911aa1837eb
vladkramarov <kramaroff01@gmail.com>
2023-03-20 22:02:44 -0600

    changed some minor details

--- src/dash_app/generate_dash_chart.py
+++ src/dash_app/generate_dash_chart.py
@@ -9,9 +9,9 @@ config.read('config.ini')
 
 
 def render_dash():
-    app = Dash(__name__, external_stylesheets=dc.EXTERNAL_STYLESHEETS)
-    app.layout = dc.create_layout()
-    @app.callback(dependencies.Output('output_chart', 'figure'),
+    dash_app = Dash(__name__, external_stylesheets=dc.EXTERNAL_STYLESHEETS)
+    dash_app.layout = dc.create_layout()
+    @dash_app.callback(dependencies.Output('output_chart', 'figure'),
                         [dependencies.Input('radio_buttons', 'value'),
                             dependencies.Input('keyword_input', 'value')])
     def update_chart(input_value_1, keyword_value):
@@ -31,7 +31,7 @@ def render_dash():
         return plotly_figure
 
     if __name__=='__main__':
-        app.run_server()
+        dash_app.run_server()
 
 render_dash()
     
