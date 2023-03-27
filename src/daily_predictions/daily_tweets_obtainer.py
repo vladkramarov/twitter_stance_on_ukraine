35c23706276a84701838ae83e3e4bfdb7c8282a1
vladkramarov <kramaroff01@gmail.com>
2023-03-26 21:27:37 -0600

    ...

--- src/daily_predictions/daily_tweets_obtainer.py
+++ src/daily_predictions/daily_tweets_obtainer.py
@@ -10,7 +10,7 @@ KEYWORDS_FOR_DAILY_TWEETS = ['ukraine', 'ukrainian']
 TWEET_LANGUAGE = 'en'
 TWEET_FILTER = '-is:retweet'
 DAILY_TWEET_LIMIT = 1000
-PAGE_TWEET_LIMIT = 100
+PAGE_TWEET_LIMIT = 99
 
 @dataclass
 class TweepyConnector:
