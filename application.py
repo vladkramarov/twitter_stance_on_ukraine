a95febebd4cad2271927e1e483fc4852043bc6de
vladkramarov <kramaroff01@gmail.com>
2023-04-10 20:01:58 -0600

    removed card viz from the app

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import dash
 from dash import dependencies
 import src.deployment.generate_chart_data as gcd
 import src.deployment.plotly_chart_components as pcc
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 application = app.server
@@ -14,13 +14,13 @@ conn, cursor = dm.connect_to_db()
 app.layout = dc.create_layout(gcd.generate_chart_data(conn))
 
 @app.callback(
-        [dependencies.Output('output_chart', 'figure'), dependencies.Output('card', 'children')],
-        [dependencies.Input('last-7-days-button', 'n_clicks'),
+        dependencies.Output('output_chart', 'figure'), 
+        [dependencies.Input('date_picker_single', 'date'),
         dependencies.Input('radio_buttons', 'value'),
          dependencies.Input('keyword_input', 'value')])
-def update_chart(last_7_days_clicks, input_value_1, keyword_value):
-    if last_7_days_clicks:
-        start_date = (datetime.now().date() - timedelta(days = 7)).strftime('%Y-%m-%d')
+def update_chart(date_value, input_value_1, keyword_value):
+    if date_value:
+        start_date = date.fromisoformat(date_value).strftime('%Y-%m-%d')
         chart_data = gcd.generate_chart_data(conn, filter_keyword = keyword_value, query_start_date=start_date)
         plotly_figure = pcc.render_full_plotly_chart(chart_data, input_value_1)
         plotly_figure.update_layout(
@@ -32,9 +32,10 @@ def update_chart(last_7_days_clicks, input_value_1, keyword_value):
     else:
         chart_data = gcd.generate_chart_data(conn, filter_keyword=keyword_value)
         plotly_figure = pcc.render_full_plotly_chart(chart_data, input_value_1)
-        dc.create_card(chart_data)
+        # dc.create_card(chart_data)
     plotly_figure.update_yaxes(title=dc.Y_AXIS_LABELS[input_value_1])
-    return plotly_figure, dc.create_card(chart_data)
+    
+    return plotly_figure
 
 if __name__ == '__main__':
     application.run(debug=True, port=8080)
--- src/deployment/dash_components.py
+++ src/deployment/dash_components.py
@@ -3,6 +3,7 @@ from typing import Callable, Dict, List
 import src.deployment.plotly_chart_components as pcc
 import src.deployment.generate_chart_data as gcd
 import dash_bootstrap_components as dbc
+import datetime
 
 EXTERNAL_STYLESHEETS = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 Y_AXIS_LABELS = {'daily_ratios': 'Daily Stance Ratio', 'cumulative_ratios': 'Cumulative Stance Ratio'}
@@ -29,26 +30,40 @@ def create_radio_items():
                     options=[{'label': Y_AXIS_LABELS[value], 'value': value} for value in Y_AXIS_LABELS.keys()
                                 ])
 
-def create_relative_date_button():
+def create_date_picker():
     '''Creates a button that allows to filter out all tweets but the ones that were created within the last 7 days'''
     return html.Div([
+        html.Div(id='output-container-date-picker-single'),
         html.Div([
-            html.Button('Last 7 Days', id='last-7-days-button', n_clicks=0)
-        ], style={'display': 'flex', 'flex-direction': 'column', 'justify-content': 'flex-end'})
-    ], style={'display': 'flex', 'justify-content': 'flex-end'})
+            html.H6('Filter tweets by the start date'),
+            dcc.DatePickerSingle(
+                id='date_picker_single', 
+                min_date_allowed=datetime.date(2023, 2, 2), 
+                max_date_allowed=datetime.date(2023, 4, 9), 
+                initial_visible_month=datetime.date(2023, 3, 10),
+                date=datetime.date(2023, 2, 2),
+                style={'display': 'flex', 'flex-direction': 'column', 'justify-content': 'center'}
+            ),
+        ],
+        style={'display': 'flex', 'justify-content': 'center', 'flex-direction': 'column', 'align-items': 'flex-end'}),
+    ],
+    style={'position': 'absolute', 'top': '10px', 'right': '15px', 'display': 'flex', 'flex-direction': 'column', 'align-items': 'flex-end'})
 
-def create_card(chart_data):
-    highest_ratio, label = gcd.select_highest_ratio_and_label(chart_data)
-    card_text = f'With {highest_ratio:.0%} {label} tweets'
-    background_color = CARD_BACKGROUND_COLORS[label]
-    return html.Div(
-        [dbc.Card([
-            dbc.CardBody([
-                html.H3(f'{label.capitalize()}', className='text-center', id='card-label'),
-                    html.P(card_text)],style={'margin': '10px'},id='card')],
-        style={'backgroundColor': background_color, 'width': '18rem'}
-    ),
-    ])
+
+
+
+# def create_card(chart_data):
+#     highest_ratio, label = gcd.select_highest_ratio_and_label(chart_data)
+#     card_text = f'With {highest_ratio:.0%} {label} tweets'
+#     background_color = CARD_BACKGROUND_COLORS[label]
+#     return html.Div(
+#         [dbc.Card(
+#                 [dbc.CardHeader("Overall Stance"),
+                 
+#                  dbc.CardBody(
+#                 [html.H3(f'{label.capitalize()}', className='text-center', id='card-label'),
+#                     html.P(card_text)], id='card',style={'backgroundColor': background_color, 'width': '24rem'})], color='dark', outline=True,
+#                     style= {'display': 'flex', 'justify-content': 'flex-end', 'align-items': 'right', 'position': 'absolute', 'top': '20px', 'right': '20px'})])
 
 def create_main_chart(chart_data, render_full_plotly_chart: Callable = pcc.render_full_plotly_chart):
     '''A function to render plotly chart with updated data'''
@@ -56,7 +71,7 @@ def create_main_chart(chart_data, render_full_plotly_chart: Callable = pcc.rende
         style={'position': 'relative'},
         children=[
             html.Div(
-                style={'position': 'absolute', 'top': '25%', 'left': '50%', 'transform': 'translate(-47%, -35%)'},
+                style={'position': 'absolute', 'top': '35%', 'left': '50%', 'transform': 'translate(-47%, -7%)'},
                 children=[
                     dcc.Graph(
                         id='output_chart',
@@ -70,7 +85,7 @@ def create_layout(chart_data):
         create_subheading(chart_data),
         create_search_bar(),
         create_radio_items(),
-        create_relative_date_button(),
-        create_card(chart_data),
+        create_date_picker(),
+        # create_card(chart_data),
         create_main_chart(chart_data)])
 
